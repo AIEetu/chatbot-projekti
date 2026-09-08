2eb127893dc11a2701ef311ca7252175cb933f05
Eetu Kosunen <eetu2030@gmail.com>
2026-09-08 13:27:47 +0300

    uusi

--- server.js
+++ server.js
@@ -88,7 +88,6 @@ app.post('/api/tarjous', async (req, res) => {
       properties: {
         ...kontaktinTiedot.properties,
         hs_lead_status: 'NEW',
-        hubspot_owner_id: HUBSPOT_OMISTAJA_ID,
       },
     };
 
